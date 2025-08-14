d1ea189bca1499866a9553205db5fccaa1068de5
Harshal Marathe <harshmarathe5@gmail.com>
2025-08-14 16:28:57 +0530

    chore(settings): configure custom user model and register users_app

--- chat_support_ai/settings.py
+++ chat_support_ai/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users_app'
 ]
 
 MIDDLEWARE = [
@@ -97,6 +98,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'UsersApp.User'
 
 
 # Internationalization
